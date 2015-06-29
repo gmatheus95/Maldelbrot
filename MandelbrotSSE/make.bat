dd874c4f5cce5a941aaee5faca99e9cb0bfc0f2f
Victor Otavio <victor_otavio205@Hotmail.com>
2015-06-28 21:30:28 -0300

    SSE + make.bat
    Nessa versão algumas operacoes foram paralelizadas usando simd sse,
    entretanto não houve nenhum ganho consideravel na eficiencia , além
    disso o metodo de gravacao no arquivo foi alterado , o arquivo é gravado
    apenas no final do programa aumentando consideravelmente a velocidade,
    como sugerido no Forum.
    O make.bat compila os dois codigos (original e modificado) e os executa
    , no final da execução de cada um é mostrado o seu tempo de execução.

---
+++ MandelbrotSSE/make.bat
@@ -0,0 +1,11 @@
+call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat"
+
+cl main_simd.cpp
+cl original.cpp
+
+cls
+echo "Rodando o codigo original..."
+original.exe
+echo "Rodando o codigo modificado..."
+main_simd.exe
+
