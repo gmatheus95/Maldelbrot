2a38f2d4d981e4dbd8439e1b8b9e2a5cd3a80b7f
Victor Otavio <victor_otavio205@Hotmail.com>
2015-07-06 14:29:54 -0300

    Editando make.bat
    - call "C:\Program Files (x86)\Microsoft Visual Studio
    12.0\VC\vcvarsall.bat" removido , como pedido pelo professor
    - O make compila o codigo original e o SSE , e roda os codigos
    original,SSE,openCL_CPU e openCL_GPU

--- make.bat
+++ make.bat
@@ -1,4 +1,3 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat"
 cd TESTES
 
 cl main_simd.cpp
@@ -13,6 +12,5 @@ echo "Rodando o codigo em OpenCL em CPU..."
 openCL_CPU.exe
 echo "Rodando o codigo em OpenCL em GPU..."
 openCL_GPU.exe
-
 
 pause
